3e7195294d6043e8906a377eb6f3c0497a061bf3
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-15 16:29:09 -0300

    feat(Home): Component "Search" from childreen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,13 +2,16 @@ import { FC } from 'react';
 
 import Header from '../../components/Header';
 import Cover from '../../components/Cover';
+import Search from '../../components/Search';
 import Card from '../../components/Card';
 import Footer from '../../components/Footer';
 
 const Home: FC = () => (
   <>
     <Header />
-    <Cover />
+    <Cover title="Explore the most powerful characters in Marvel">
+      <Search />
+    </Cover>
     <Card />
     <Footer />
   </>
