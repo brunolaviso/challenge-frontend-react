ece928e16c9b863913de2e614e91cd22fbd28e78
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-15 16:28:17 -0300

    feat(Header): btn "your team" navigate to router

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import css from './Header.module.scss';
 
 import logo from '../../assets/logo.svg';
@@ -9,13 +10,14 @@ const Header: FC = () => (
     <div className={`container ${css.M__Itens}`}>
       <h1 className={css.Mi__Title}>
         <img className={css.Mit__Img} src={logo} alt="Iron man mask" />
-        {' '}
         Marvel Strike Team
       </h1>
-      <button className={css.Mi__Btn} type="button">
-        Your Team
-        <img className={css.Mib__Img} src={users} alt="Users icon" />
-      </button>
+      <Link className={css.Link} to="/team">
+        <button className={css.Mi__Btn} type="button">
+          Your Team
+          <img className={css.Mib__Img} src={users} alt="Users icon" />
+        </button>
+      </Link>
     </div>
   </div>
 );
