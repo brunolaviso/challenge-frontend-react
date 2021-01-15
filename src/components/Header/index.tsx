06b7950729fc3e4959accf3b964e157e5915ebdf
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-15 15:34:20 -0300

    refactor: eslint refactor airbnb default

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,23 @@
+import { FC } from 'react';
 import css from './Header.module.scss';
 
 import logo from '../../assets/logo.svg';
 import users from '../../assets/users.svg';
 
-const Header:React.FC = () => (
+const Header:FC = () => (
   <div className={css.Menu}>
     <div className={`container ${css.M__Itens}`}>
-      <h1 className={css.Mi__Title}><img className={css.Mit__Img} src={logo} alt="Iron man mask" /> Marvel Stike Team</h1>
-      <button className={css.Mi__Btn}>Your Team<img className={css.Mib__Img} src={users} alt="Users icon" /></button>
+      <h1 className={css.Mi__Title}>
+        <img className={css.Mit__Img} src={logo} alt="Iron man mask" />
+        {' '}
+        Marvel Strike Team
+      </h1>
+      <button className={css.Mi__Btn} type="button">
+        Your Team
+        <img className={css.Mib__Img} src={users} alt="Users icon" />
+      </button>
     </div>
   </div>
-)
+);
 
 export default Header;
