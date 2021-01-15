bb52be278ec1d553502f2fa02153bb107062104d
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-15 16:29:47 -0300

    feat(team): initial structure

--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -1,7 +1,15 @@
 import { FC } from 'react';
 
+import Header from '../../components/Header';
+import Cover from '../../components/Cover';
+import Footer from '../../components/Footer';
+
 const Team: FC = () => (
-  <h1>Team</h1>
+  <>
+    <Header />
+    <Cover title="Discover all comics this character took part in" />
+    <Footer />
+  </>
 );
 
 export default Team;
