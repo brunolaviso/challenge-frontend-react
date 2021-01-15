d5c89eab21928a77af780ae0e1101adc2626520f
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-15 15:48:55 -0300

    refactor: remove search from the cover

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -1,16 +1,10 @@
 import { FC } from 'react';
 import css from './Cover.module.scss';
 
-import search from '../../assets/search_24px.svg';
-
 const Cover: FC = () => (
   <div className={css.Cover}>
     <div className={`container ${css.C__Itens}`}>
       <h2 className={css.CI__Title}>Explore the most powerful characters in Marvel</h2>
-      <div className={css.CI__Form}>
-        <input className={css.CIF__Textbox} type="text" />
-        <button className={css.CIF__Btn} type="button"><img src={search} alt="Search" /></button>
-      </div>
     </div>
   </div>
 );
