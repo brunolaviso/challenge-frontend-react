ad20893b32059882c4f05575f85f000906911021
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-18 11:02:05 -0300

    feat(home): home page structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,13 +6,32 @@ import Search from '../../components/Search';
 import Card from '../../components/Card';
 import Footer from '../../components/Footer';
 
+import css from './Home.module.scss';
+
 const Home: FC = () => (
   <>
     <Header />
     <Cover title="Explore the most powerful characters in Marvel">
       <Search />
     </Cover>
-    <Card />
+    <main className="container">
+      <div className={css.Info}>
+        <h2 className={css.I__Title}>Characters</h2>
+        <small className={css.I__Results}># results</small>
+      </div>
+      <div className={css.Main}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </main>
     <Footer />
   </>
 );
