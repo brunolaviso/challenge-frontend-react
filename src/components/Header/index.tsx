f1b51f04f924964c375f921d537fda8fce939eaa
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-18 11:41:44 -0300

    feat(Header): add link in logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,10 +8,12 @@ import users from '../../assets/users.svg';
 const Header: FC = () => (
   <div className={css.Menu}>
     <div className={`container ${css.M__Itens}`}>
-      <h1 className={css.Mi__Title}>
-        <img className={css.Mit__Img} src={logo} alt="Iron man mask" />
-        Marvel Strike Team
-      </h1>
+      <Link className={css.Link} to="/">
+        <h1 className={css.Mi__Title}>
+          <img className={css.Mit__Img} src={logo} alt="Iron man mask" />
+          Marvel Strike Team
+        </h1>
+      </Link>
       <Link className={css.Link} to="/team">
         <button className={css.Mi__Btn} type="button">
           Your Team
