280bd6672358f9866d3c599cab2fa9de93e628c8
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-18 23:44:55 -0300

    feat(card): add props 'name, description, img' and fix styles

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,22 +1,26 @@
 import { FC } from 'react';
 
-import spiderMan from '../../assets/characters/spider-man.svg';
 import team from '../../assets/team.svg';
 
 import css from './Card.module.scss';
 
-const Card: FC = () => (
+interface ICard {
+  name: string,
+  description: string,
+  thumbnail: string
+}
+
+const Card: FC<ICard> = ({ name, description, thumbnail }) => (
   <div className={css.Card}>
-    <img src={spiderMan} alt="Spider Man" />
+    <div className={css.C__Wrappad}>
+      <img className={css.CW__Img} src={thumbnail} alt="Thumbnail of character" />
+    </div>
     <button className={css.C__Icon} type="button">
       <img src={team} alt="Add in your team" />
     </button>
     <div className={css.C__Details}>
-      <h3 className={css.CD__Title}>Spider Man</h3>
-      <p className={css.CD__Description}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, corporis
-        facilis. Sit esse vero explicabo.
-      </p>
+      <h3 className={css.CD__Title}>{name}</h3>
+      <p className={css.CD__Description}>{description}</p>
     </div>
   </div>
 );
