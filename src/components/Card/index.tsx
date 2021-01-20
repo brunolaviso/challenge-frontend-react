de1a5403dff2776aea29494ba4f6cae4a6b883dc
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-20 09:30:19 -0300

    feat(Card): add react memo and change semantic tag

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 
 import team from '../../assets/team.svg';
 
@@ -11,18 +11,18 @@ interface ICard {
 }
 
 const Card: FC<ICard> = ({ name, description, thumbnail }) => (
-  <div className={css.Card}>
+  <section className={css.Card}>
     <div className={css.C__Wrappad}>
       <img className={css.CW__Img} src={thumbnail} alt="Thumbnail of character" />
     </div>
-    <button className={css.C__Icon} type="button">
+    <button className={css.C__Icon} type="submit">
       <img src={team} alt="Add in your team" />
     </button>
     <div className={css.C__Details}>
       <h3 className={css.CD__Title}>{name}</h3>
       <p className={css.CD__Description}>{description}</p>
     </div>
-  </div>
+  </section>
 );
 
-export default Card;
+export default memo(Card);
