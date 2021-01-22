0ddcbc30cdc4d5aba9dbdaf8bfabe063751a2466
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-22 11:53:49 -0300

    fix(CharacterComics): rename page

--- src/pages/CharacterComics/index.tsx
+++ src/pages/CharacterComics/index.tsx
@@ -3,11 +3,19 @@ import { FC } from 'react';
 import Header from '../../components/Header';
 import Cover from '../../components/Cover';
 import Footer from '../../components/Footer';
+import Comic from '../../components/Comic';
+import Character from '../../components/Character';
+
+import css from './CharacterComics.module.scss';
 
 const CharacterProfile: FC = () => (
   <>
     <Header />
     <Cover title="Discover all comics this character took part in" />
+    <main className={`container ${css.Main}`}>
+      <Character />
+      <Comic />
+    </main>
     <Footer />
   </>
 );
