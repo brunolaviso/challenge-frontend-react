c35b7d2bdee79445138ac54bf28081aa4c827e72
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-22 11:54:21 -0300

    refactor(routes): rename routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Team from '../pages/Team';
-import CharacterProfile from '../pages/CharacterProfile';
+import CharacterProfile from '../pages/CharacterComics';
 
 const Router: FC = () => (
   <BrowserRouter>
