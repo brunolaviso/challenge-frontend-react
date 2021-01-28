15925a3817f69c5d4075818abccb1538e94005d9
Bruno Laviso <bruno.laviso@hotmail.com>
2021-01-28 14:18:59 -0300

    feat(app): define theme to skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
+import { SkeletonTheme } from 'react-loading-skeleton';
 
 import Router from './routes';
 
 const App: FC = () => (
-  <Router />
+  <SkeletonTheme color="#cacaca" highlightColor="#e0e0e0">
+    <Router />
+  </SkeletonTheme>
 );
 
 export default App;
