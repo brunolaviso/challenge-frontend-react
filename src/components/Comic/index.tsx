48ffa286552f6550ef33b67693a314d3d2ef2ba4
Bruno Laviso <bruno.laviso@hotmail.com>
2021-02-02 15:11:13 -0300

    feat(cover): create structure and styles

--- src/components/Comic/index.tsx
+++ src/components/Comic/index.tsx
@@ -1,36 +1,41 @@
 import { FC } from 'react';
 
-import spiderManComic from '../../assets/comics/spider-man.svg';
+// import spiderManComic from '../../assets/comics/spider-man.svg';
 import dotCircle from '../../assets/dot-circle.svg';
 
 import css from './Comic.module.scss';
 
-const Comic: FC = () => (
+interface IComics {
+  title: string,
+  description: string,
+  pageCount: number,
+  price: number
+  thumbnail: {
+    extension: string,
+    path: string
+  }
+}
+
+const Comic: FC<IComics> = ({
+  title, description, pageCount, price, thumbnail: { path, extension },
+}) => (
   <div className={css.Comic}>
-    <img className={css.C__Img} src={spiderManComic} alt="" />
+    <img className={css.C__Img} src={`${path}/standard_fantastic.${extension}`} alt="" />
     <div className={css.C__Info}>
-      <h2 className={css.CI__Title}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Illum fugiat quasi velit ipsum illo placeat perspiciatis fugit delectus fuga, doloribus.
-      </h2>
+      <h2 className={css.CI__Title}>{title}</h2>
       <div className={css.CI__Details}>
         <p>01/01/1990</p>
         <img src={dotCircle} alt="" />
-        <p>47 pages</p>
+        <p>{`${pageCount} pages`}</p>
         <img src={dotCircle} alt="" />
-        <p>U$0.90</p>
+        <p>{`US$ ${price}`}</p>
       </div>
       <div className={css.CI__Description}>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Perspiciatis veniam nihil, commodi assumenda quibusdam,
-          eaque ut ad debitis quidem officia beatae facilis quas,
-          dignissimos ex repellat dolor obcaecati adipisci dicta?
-
-        </p>
+        {description
+          ? <p>{description}</p>
+          : <p className={css.CID__NoDescription}>Has no description.</p>}
       </div>
     </div>
   </div>
 );
-
 export default Comic;
