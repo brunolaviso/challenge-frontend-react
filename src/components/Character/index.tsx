8e0ec95e73ea636a1768cacc347bb9997d06c341
Bruno Laviso <bruno.laviso@hotmail.com>
2021-02-02 15:12:18 -0300

    feat(character): create structure and styles from card character of comics page

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,19 +1,30 @@
 import { FC } from 'react';
-
-import spiderMan from '../../assets/characters/spider.png';
-
 import css from './Character.module.scss';
 
-const Character: FC = () => (
+interface ICharacter {
+  id: number,
+  name: string,
+  description: string,
+  thumbnail: {
+    extension: string,
+    path: string
+  }
+}
+
+const Character: FC<ICharacter> = ({
+  name, description, thumbnail: { path, extension },
+}) => (
   <div className={css.Character}>
-    <img className={css.C__Img} src={spiderMan} alt="" />
+    <img
+      className={css.C__Img}
+      src={`${path}/standard_fantastic.${extension}`}
+      alt=""
+    />
     <div className={css.C__Info}>
-      <h1 className={css.CI__Title}>Spider-Man</h1>
-      <p className={css.CI__Description}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Hic illum facere debitis obcaecati assumenda quaerat dolores, itaque facilis
-        voluptatum iste delectus, ut quia nesciunt recusandae voluptatem laborum inventore.
-      </p>
+      <h1 className={css.CI__Title}>{name}</h1>
+      {description.length > 0
+        ? <p className={css.CI__Description}>{description}</p>
+        : <p className={css.CI__NoDescription}>Has no description.</p>}
     </div>
   </div>
 );
